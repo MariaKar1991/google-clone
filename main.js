7f6393673a36c1d739cbb569d139ead76954cf37
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:04:57 +0200

    Update main.2.js

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+// Get the search input element
 const searchInput = document.querySelector("#search-input");
 
+// Add an event listener for the "Enter" key
 searchInput.addEventListener("keydown", function (event) {
+  // Check if the pressed key is "Enter"
   if (event.code === "Enter") {
+    // Call the search function when "Enter" is pressed
     search();
   }
 });
 
+// Function to perform the search
 function search() {
+  // Get the user input from the search input field
   const input = searchInput.value;
 
+  // Redirect the user to the Google search results page with the entered query
   window.location.href =
     "https://www.google.com/search?q=" +
     input +
